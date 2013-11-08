420a3d1855f413f16fa17b32838b9f86fb3cc8d7
JesusGavilan <jegavilgar@gmail.com>
2013-11-08 02:10:36 +0100

    Modified topology in order to have common atributes with the data that floodlight can provides.

--- graphdb/src/main/java/dxat/project/draft/graphdb/GraphDBEmbedded.java
+++ graphdb/src/main/java/dxat/project/draft/graphdb/GraphDBEmbedded.java
@@ -2,7 +2,7 @@ package dxat.project.draft.graphdb;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
+//import java.util.HashMap;
 
 import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.GraphDatabaseService;
@@ -12,9 +12,11 @@ import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.RelationshipType;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.graphdb.index.Index;
 import org.neo4j.graphdb.traversal.Evaluators;
 import org.neo4j.graphdb.traversal.TraversalDescription;
 import org.neo4j.graphdb.traversal.Traverser;
+//import org.neo4j.kernel.ListIndexIterable;
 import org.neo4j.kernel.Traversal;
 
 
@@ -24,10 +26,12 @@ public class GraphDBEmbedded {
 	//private static final String DB_PATH ="/var/lib/neo4j/data/graph.db";
 	//For Windows
 	//private static final String DB_PATH ="/path";
+	
 	private static final String DB_PATH= "target/graph-network";
 	private GraphDatabaseService graphDb;
-	private long SDNNodeId;
-	private HashMap<String, Node> listIfaceDevices = new HashMap<String, Node>();
+	//private long SDNNodeId;
+	private static Index<Node> listIfaceDevices; //In order to make indexes over the nodes
+	//private HashMap<String, Node> listIfaceDevices = new HashMap<String, Node>();
 	
 	//LIST OF RELATION TYPES
 	private static enum RelTypes implements RelationshipType{
@@ -39,6 +43,7 @@ public class GraphDBEmbedded {
 	{
 		deleteFileOrDirectory( new File(DB_PATH));
 		graphDb = new GraphDatabaseFactory().newEmbeddedDatabase(DB_PATH);
+		listIfaceDevices = graphDb.index().forNodes("devices");
 		registerShutdownHook();
 		createNodespace();
 	}
@@ -56,55 +61,77 @@ public class GraphDBEmbedded {
 			//Create base network node
 			Node network = graphDb.createNode();
 			network.setProperty("name", "SDNnetwork");
-			SDNNodeId = network.getId();
+			network.setProperty("inventoryId", "SDNnetwork");
+			listIfaceDevices.add(network, "inventoryId", "SDNnetwork");
+			//SDNNodeId = network.getId();
+			
+			//Create controller node
+			Node controller = graphDb.createNode();
+			controller.setProperty("inventoryId", "SDNcontroller");
+			listIfaceDevices.add(controller, "inventoryId", "SDNcontroller");
+			
+			//Network-controller relationship
+			network.createRelationshipTo(controller, RelTypes.ELEMENT);
+			
+			//Create controller interfaces
+			for(int i=0;i<4;i++){
+				Node ports = CreateNodeIfaces("CT_P"+ Integer.toString(i) , "CT");
+				CreateRelDevToIfaces(controller, ports);
+				listIfaceDevices.add(ports, "inventoryId", (String) ports.getProperty("inventoryId"));
+			}
+						
+			
 			int numIfaces;
 			
 			//Create switch and ports nodes
 			for(int i=0;i<4;i++){
 			
 				Node sw = CreateNodeDevices(Integer.toString(i), "SW",5);
+				listIfaceDevices.add(sw, "inventoryId", (String) sw.getProperty("inventoryId"));
 				//Define network-->element relationship
 				network.createRelationshipTo(sw, RelTypes.ELEMENT);
-				numIfaces = (Integer) sw.getProperty("interfaces");
+				numIfaces = (Integer) sw.getProperty("ifaces");
 				for(int j=0; j<numIfaces; j++){
 					Node iface = CreateNodeIfaces("SW-" + Integer.toString(i) + "_P" + Integer.toString(j), "SW");
 					//Define sw-->port relationship;
 					CreateRelDevToIfaces(sw, iface);
-					//Added DevicePort to HashMap
-					listIfaceDevices.put((String) iface.getProperty("inventoryId"), iface);
+					//Added DevicePort to index
+					listIfaceDevices.add(iface, "inventoryId", (String) iface.getProperty("inventoryId")); 
 				}
 			}
 			
 			//Create pc nodes
 			for(int i=0; i<4;i++){
 				Node pc = CreateNodeDevices(Integer.toString(i), "PC", 1);
+				listIfaceDevices.add(pc,"inventoryId", (String) pc.getProperty("inventoryId"));
 				Node iface = CreateNodeIfaces("PC-" + Integer.toString(i)+ "_P0","PC");
 				//Define pc-->port relationship
 				CreateRelDevToIfaces(pc, iface);
 				//Added DevicePort to HashMap
-				listIfaceDevices.put((String) iface.getProperty("inventoryId"), iface);
+				listIfaceDevices.add(iface,"inventoryId", (String) iface.getProperty("inventoryId"));
 			}
 			
 			//CREATE LINKS**********
-			System.out.println("*************" + listIfaceDevices.keySet());
+			//Creating SW (ifaces) to Controller (ifaces) links
+			CreateLinks(listIfaceDevices.get("inventoryId","SW-0_P0").getSingle(), listIfaceDevices.get("inventoryId", "CT_P0").getSingle());
+			CreateLinks(listIfaceDevices.get("inventoryId","SW-1_P0").getSingle(), listIfaceDevices.get("inventoryId", "CT_P1").getSingle());
+			CreateLinks(listIfaceDevices.get("inventoryId","SW-2_P0").getSingle(), listIfaceDevices.get("inventoryId", "CT_P2").getSingle());
+			CreateLinks(listIfaceDevices.get("inventoryId","SW-3_P0").getSingle(), listIfaceDevices.get("inventoryId", "CT_P3").getSingle());
 			
-			CreateLinks(listIfaceDevices.get("PC-0_P0"),listIfaceDevices.get("SW-0_P0"));
-			CreateLinks(listIfaceDevices.get("PC-1_P0"),listIfaceDevices.get("SW-0_P1"));
-			CreateLinks(listIfaceDevices.get("SW-0_P2"),listIfaceDevices.get("SW-1_P0"));
-			CreateLinks(listIfaceDevices.get("SW-0_P3"),listIfaceDevices.get("SW-2_P0"));
-			CreateLinks(listIfaceDevices.get("SW-1_P1"),listIfaceDevices.get("SW-2_P1"));
-			CreateLinks(listIfaceDevices.get("SW-1_P2"),listIfaceDevices.get("SW-3_P0"));
-			CreateLinks(listIfaceDevices.get("SW-2_P2"),listIfaceDevices.get("SW-3_P1"));
-			CreateLinks(listIfaceDevices.get("SW-3_P2"),listIfaceDevices.get("PC-2_P0"));
-			CreateLinks(listIfaceDevices.get("SW-3_P3"),listIfaceDevices.get("PC-3_P0"));			
+			//Creating SW (ifaces) to SW (ifaces) links
+			CreateLinks(listIfaceDevices.get("inventoryId","PC-0_P0").getSingle(),listIfaceDevices.get("inventoryId","SW-0_P1").getSingle());
+			CreateLinks(listIfaceDevices.get("inventoryId","PC-1_P0").getSingle(),listIfaceDevices.get("inventoryId","SW-0_P2").getSingle());
+			CreateLinks(listIfaceDevices.get("inventoryId","SW-0_P3").getSingle(),listIfaceDevices.get("inventoryId","SW-2_P2").getSingle());
+			CreateLinks(listIfaceDevices.get("inventoryId","SW-0_P4").getSingle(),listIfaceDevices.get("inventoryId","SW-1_P1").getSingle());
+			CreateLinks(listIfaceDevices.get("inventoryId","SW-1_P2").getSingle(),listIfaceDevices.get("inventoryId","SW-3_P2").getSingle());
+			CreateLinks(listIfaceDevices.get("inventoryId","SW-1_P3").getSingle(),listIfaceDevices.get("inventoryId","SW-2_P1").getSingle());
+			CreateLinks(listIfaceDevices.get("inventoryId","SW-2_P3").getSingle(),listIfaceDevices.get("inventoryId","SW-3_P1").getSingle());
+			CreateLinks(listIfaceDevices.get("inventoryId","SW-3_P3").getSingle(),listIfaceDevices.get("inventoryId","PC-2_P0").getSingle());
+			CreateLinks(listIfaceDevices.get("inventoryId","SW-3_P4").getSingle(),listIfaceDevices.get("inventoryId","PC-3_P0").getSingle());			
 			
 			tx.success();	
 		}
-		/*
-		catch (Exception e)
-		{
-			tx.failure();
-		}*/
+		
 		finally
 		{
 			tx.finish();
@@ -118,7 +145,7 @@ public class GraphDBEmbedded {
 		device.setProperty("inventoryId", type + "-" + name);
 		device.setProperty("status", true);
 		device.setProperty("type", type );
-		device.setProperty("interfaces", ports);
+		device.setProperty("ifaces", ports);
 		if(type=="SW" || type=="RT")	device.setProperty("model", "RouterBOARD 750GL");
 		else device.setProperty("model", "PC");
 		return device;
@@ -128,10 +155,11 @@ public class GraphDBEmbedded {
 		//id---> sw0_p0, pc0_p1....
 		Node iface = graphDb.createNode();
 		iface.setProperty("inventoryId", name );
-		iface.setProperty("status", true);
+		iface.setProperty("status", false);
+		iface.setProperty("enabled", false);
 		iface.setProperty("mac", "00:34:22:33:33:69");
 		if(type=="RT" || type=="PC") iface.setProperty("ip", "66.66.66.66");
-		iface.setProperty("ethernet", 100);//10/100/1000 Mbps
+		iface.setProperty("currentSpeed", 100);//10/100/1000 Mbps
 		return iface;
 	}
 	
@@ -144,8 +172,8 @@ public class GraphDBEmbedded {
 		Relationship link = interface1.createRelationshipTo(interface2, RelTypes.LINK);
 		link.setProperty("id", (String) interface1.getProperty("inventoryId") + "-"+ (String) interface2.getProperty("inventoryId"));
 		
-		int capacity1 = (Integer) interface1.getProperty("ethernet");
-		int capacity2 = (Integer) interface2.getProperty("ethernet");
+		int capacity1 = (Integer) interface1.getProperty("currentSpeed");
+		int capacity2 = (Integer) interface2.getProperty("currentSpeed");
 		int bandwidth;
 		
 		if (capacity1>capacity2) bandwidth = capacity2;
@@ -155,18 +183,19 @@ public class GraphDBEmbedded {
 		
 	}
 	
-	private Node getNetworkNode()
+	private Node getControllerNetwork()
 	{
-		return graphDb.getNodeById(SDNNodeId);
+		return listIfaceDevices.get("inventoryId", "SDNcontroller").getSingle();
+		
 	}
 	
 	public String printNetworkElements(){
 		Transaction tx = graphDb.beginTx();
 		try
 		{
-			Node networkNode = getNetworkNode();
+			Node networkNode = getControllerNetwork();
 			int numberOfElements = 0;
-			String output = networkNode.getProperty("name") + "'s elements:\n";
+			String output = networkNode.getProperty("inventoryId") + "'s elements:\n";
 			Traverser elementsTraverser = getElements(networkNode);
 			
 			for (Path elementPath : elementsTraverser){
@@ -188,7 +217,7 @@ public class GraphDBEmbedded {
 	private static Traverser getElements( final Node element){
 		TraversalDescription td = Traversal.description()
 				.breadthFirst()
-				.relationships(RelTypes.ELEMENT, Direction.OUTGOING)
+				.relationships(RelTypes.HAS, Direction.OUTGOING)
 				.evaluator(Evaluators.excludeStartPosition());
 		td.traverse(element);
 		return td.traverse(element);
